5b1dd441dd83134abfaadd9d46751827c69b5a2d
yang hee jung <puuurm@naver.com>
2019-05-22 23:40:55 +0900

    Add: Fish

---
+++ Codility/Fish.playground/Contents.swift
@@ -0,0 +1,37 @@
+import Cocoa
+
+public func solution(_ A : inout [Int], _ B : inout [Int]) -> Int {
+
+    var aliveCount: Int = 0
+    var downStrems: [Int] = []
+
+    for index in 0..<A.count {
+        if B[index] == 0 {
+            if downStrems.isEmpty {
+                aliveCount += 1
+            } else {
+                while !downStrems.isEmpty {
+                    if downStrems.last! > A[index] {
+                        break
+                    } else {
+                        downStrems.removeLast()
+                    }
+                }
+                if downStrems.isEmpty {
+                    aliveCount += 1
+                }
+            }
+        } else {
+            downStrems.append(A[index])
+        }
+    }
+
+    return aliveCount + downStrems.count
+
+}
+
+var A = [4,2,3,4,5]
+var B = [0,0,0,1,0]
+
+print(solution(&A, &B))
+
